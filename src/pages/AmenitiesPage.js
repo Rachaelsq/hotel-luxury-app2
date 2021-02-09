1f6e23f3e46577e1245025bc259edeb1da9d3b19
Rachaelsq <intuitiveharmonymedicine@gmail.com>
2021-02-08 21:19:01 -0600

    more work on image gallery

--- src/pages/AmenitiesPage.js
+++ src/pages/AmenitiesPage.js
@@ -21,45 +21,56 @@ export default function AmenitiesPage() {
     return (
         <>
         <h3>amenities </h3>
-            <div class="container" id="AmenitiesBigContainer">
+            <div class="container-fluid" id="AmenitiesBigContainer">
 
                 <div class="col-lg" id="firstColumn">
                     <div class="row">
 
                         <div class="col-lg ">
-                            <h3>column one</h3>
-                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid" alt="Responsive image" />
-                        </div>
-                        <div class="col-lg " id="middleColumn">
-                            <h3>column two</h3>
-                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid" alt="Responsive image" />
-                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid middleBottom" alt="Responsive image" />
-                        </div>
-
-
-
-                        <div class="col-lg" id="imgColumns">
-                            <div class="row row-no-padding">
+                            
+                            <div class="row row-no-padding" id="topSideImgId">
                                 <h3>column three</h3>
-                                <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid" alt="Responsive image" />
+                                <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid topSideImg d-flex w-100" alt="Responsive image" />
                             </div>
 
                                 <div class="" id="bottomAmenitiesId">
                                     <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid bottomImg" alt="Responsive image" />
                                     <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid bottomImg" alt="Responsive image" />
+                                </div>
+                        </div>
+                        
+                        
 
+
+
+
+                        
+                        <div class="col-lg " id="middleColumn">
+                        <div class="row row-no-padding">
+
+                            <h3>column two</h3>
+                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid d-flex w-100" alt="Responsive image" />
+                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid middleBottom" alt="Responsive image" />
+                        </div>
+                        </div>
+
+
+
+                        <div class="col-lg" id="lastColumn">
+                            <div class="row row-no-padding">
+                                <h3>column three</h3>
+                                <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid d-flex w-100" alt="Responsive image" />
+                            </div>
+
+                                <div class="" id="bottomAmenitiesId">
+                                    <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid bottomImg" alt="Responsive image" />
+                                    <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid bottomImg" alt="Responsive image" />
                                 </div>
                             
                         </div>
 
 
-                        {/*   <div class="row bottomAmenitiesClass">
-                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid bottomImg" alt="Responsive image" />
-                        </div>
-
-                        <div class="row bottomAmenitiesClass">
-                            <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class="img-fluid bottomImg" alt="Responsive image" />
-                        </div> */}
+                        
 
                         </div>
 
