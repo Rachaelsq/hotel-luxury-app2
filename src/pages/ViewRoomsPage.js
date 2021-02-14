5da40f85a41d01e3fdc6f65156e8714685eaae50
Rachaelsq <intuitiveharmonymedicine@gmail.com>
2021-02-14 14:50:38 -0600

    view rooms page done

--- src/pages/ViewRoomsPage.js
+++ src/pages/ViewRoomsPage.js
@@ -19,40 +19,68 @@ export default function ViewRoomsPage() {
         <div className="App">          
             <div class="container-fluid" id="homeImgRow">
                     <div class="row">
+
                         <div class="col-lg ">
                             <h2 class="viewRmTitle">The Cedar</h2>
-                            <img src="https://images.unsplash.com/photo-1505773278895-5efa7b11679a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=836&q=80" class="img-fluid" alt="Responsive image" />
+                            <div id="imgBtnContainer">
+                                <a href="www.google.com">
+                                    <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
+                                        <button type="button" class="btn btn-info visitBtn" id="">Visit the cedar</button>
+                                </a>
+                            </div>
                         </div>
 
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Willow</h2>
-                            <img src="https://images.unsplash.com/photo-1603198132334-5478a1ec5bc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid " alt="Responsive image" />
+                            <h2 class="viewRmTitle">The Willow</h2>
+                            <div id="imgBtnContainer">
+                            <a href="www.google.com">
+                                <img src="https://images.unsplash.com/photo-1603198132334-5478a1ec5bc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
+                                    <button type="button" class="btn btn-info visitBtn" id="">Visit the cedar</button>
+                            </a>
+                            </div>
                         </div>
+
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Sagebush</h2>
-                            <img src="https://images.unsplash.com/photo-1574197634772-f0de0d41a02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid " alt="Responsive image" />
+                            <h2 class="viewRmTitle">The Sagebush</h2>
+                            <div id="imgBtnContainer">
+                            <a href="www.google.com">
+                                <img src="https://images.unsplash.com/photo-1574197634772-f0de0d41a02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
+                                <button type="button" class="btn btn-info visitBtn" id="">Visit the cedar</button>
+                            </a>
+                            </div>
                         </div>
+
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Aloe</h2>
-                            <img src="https://images.unsplash.com/photo-1594900240017-d99eeb660287?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid " alt="Responsive image" />
+                            <h2 class="viewRmTitle">The Aloe</h2>
+                            <div id="imgBtnContainer">
+                            <a href="www.google.com">
+                                <img src="https://images.unsplash.com/photo-1594900240017-d99eeb660287?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
+                                <button type="button" class="btn btn-info visitBtn" id="">Visit the cedar</button>
+                            </a>
+                            </div>
                         </div>
                     </div>
+
+
+
+
+
                     <div class="row">
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Aspen</h2>
-                            <img src="https://images.unsplash.com/photo-1588743903237-329f9da76dbe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid" alt="Responsive image" />
+                        <h2 class="viewRmTitle">The Aspen <h6>(coming soon)</h6></h2>
+                            <img src="https://images.unsplash.com/photo-1588743903237-329f9da76dbe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
                         </div>
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Birch</h2>
-                            <img src="https://images.unsplash.com/photo-1499916078039-922301b0eb9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid " alt="Responsive image" />
+                        <h2 class="viewRmTitle">The Birch <h6>(coming soon)</h6></h2>
+                            <img src="https://images.unsplash.com/photo-1499916078039-922301b0eb9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
                         </div>
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Elm</h2>
-                            <img src="https://images.unsplash.com/photo-1584537319035-d6f4fb63f4c4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid " alt="Responsive image" />
+                        <h2 class="viewRmTitle">The Elm <h6>(coming soon)</h6></h2>
+                            <img src="https://images.unsplash.com/photo-1584537319035-d6f4fb63f4c4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg " alt="Responsive image" />
                         </div>
                         <div class="col-lg ">
-                        <h2 class="viewRmTitle">The Oak</h2>
-                            <img src="https://images.unsplash.com/photo-1602497861417-a1de2b405bea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=777&q=80" class="img-fluid " alt="Responsive image" />
+                        <h2 class="viewRmTitle">The Oak <h6>(coming soon)</h6></h2>
+                            <img src="https://images.unsplash.com/photo-1522107177-01884fcfa2bb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid viewRmImg" alt="Responsive image" />
                         </div>
                     </div>
 </div>
