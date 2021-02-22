5edec2725fc2d7380a9bcbecb076aaea5bb03afc
Rachaelsq <rachaelqsimone@gmail.com>
2021-02-22 17:15:25 -0600

    frame for reservation page

--- src/pages/MakeAReservationPage.js
+++ src/pages/MakeAReservationPage.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //pages
 //components
 
-
 /* 
 ============
 END IMPORTS
@@ -13,11 +12,50 @@ END IMPORTS
 
 
 
-export default function MakeAReservationPage() {
-    return (
-        <div>
-            <div>    this is make a reservation</div>
 
+export default function MakeAReservationPage() {
+  return (
+    <div className="c">          
+      <div class="row" id="">  
+        <div class=" jumbotron container-fluid" id="BookRmJumboId">
+          <div class="col-lg">
+            <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+          </div>
+          <div class="col-lg">
+            <h5 class="card-title">The Cedar</h5>
+              <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
+              <a href="#" class="card-link">view the Cedar</a>
+              <button type="button" class="btn btn-info " id="">Reserve now</button>
+          </div>
         </div>
+          <div class="row" id="">  
+            <div class=" jumbotron container-fluid" id="BookRmJumboId">
+              <div class="col-lg">
+                <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+              </div>
+              <div class="col-lg">
+                <h5 class="card-title">The Cedar</h5>
+                  <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
+                  <a href="#" class="card-link">view the Cedar</a>
+                  <button type="button" class="btn btn-info " id="">Reserve now</button>
+                  </div>
+                  </div>
+                                  
+      
+        
+
+
+      </div>
+    </div>
+  </div>
+
     );
 }
+
+
+
+
+
+
+
+
