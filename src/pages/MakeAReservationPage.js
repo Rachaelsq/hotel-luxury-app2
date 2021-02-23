fef1a80b95c274f00bced2be1ee9b9fbcf7a7d4b
Rachaelsq <rachaelqsimone@gmail.com>
2021-02-22 19:37:52 -0600

    adding bootstrap modal

--- src/pages/MakeAReservationPage.js
+++ src/pages/MakeAReservationPage.js
@@ -15,9 +15,9 @@ END IMPORTS
 
 export default function MakeAReservationPage() {
   return (
-    <div className="c">          
-      <div class="row" id="">  
-        <div class=" jumbotron container-fluid" id="BookRmJumboId">
+    <div class=" container-fluid reserveContainer">          
+      <div class="" id="">  
+        <div class=" jumbotron container-fluid BookRmJumbo" id="">
           <div class="col-lg">
             <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
           </div>
@@ -28,24 +28,49 @@ export default function MakeAReservationPage() {
               <button type="button" class="btn btn-info " id="">Reserve now</button>
           </div>
         </div>
-          <div class="row" id="">  
-            <div class=" jumbotron container-fluid" id="BookRmJumboId">
-              <div class="col-lg">
-                <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
-              </div>
-              <div class="col-lg">
-                <h5 class="card-title">The Cedar</h5>
-                  <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
-                  <a href="#" class="card-link">view the Cedar</a>
-                  <button type="button" class="btn btn-info " id="">Reserve now</button>
-                  </div>
-                  </div>
+        <div class="" id="">  
+          <div class=" jumbotron container-fluid BookRmJumbo" id="">
+            <div class="col-lg">
+              <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+            </div>
+            <div class="col-lg">
+              <h5 class="card-title">The Cedar</h5>
+              <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
+              <a href="#" class="card-link">view the Cedar</a>
+              <button type="button" class="btn btn-info " id="">Reserve now</button>
+            </div>
+          </div>
+        </div>
+        <div class="" id="">  
+          <div class=" jumbotron container-fluid BookRmJumbo" id="">
+            <div class="col-lg">
+              <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+            </div>
+            <div class="col-lg">
+              <h5 class="card-title">The Cedar</h5>
+              <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
+              <a href="#" class="card-link">view the Cedar</a>
+              <button type="button" class="btn btn-info " id="">Reserve now</button>
+            </div>
+          </div>
+        </div>
+        <div class="" id="">  
+          <div class=" jumbotron container-fluid BookRmJumbo" id="">
+            <div class="col-lg">
+              <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+            </div>
+            <div class="col-lg">
+              <h5 class="card-title">The Cedar</h5>
+              <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
+              <a href="#" class="card-link">view the Cedar</a>
+              <button type="button" class="btn btn-info " id="">Reserve now</button>
+            </div>
+          </div>
+        </div>
                                   
       
         
 
-
-      </div>
     </div>
   </div>
 
