3947a660205423631b9ca55fe27ac63363d9c87f
Rachaelsq <rachaelqsimone@gmail.com>
2021-02-28 16:42:05 -0600

    added modal
    added modal to reservation page

---
+++ src/components/ReservationModal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+//pages
+import MakeAReservationPage from "../pages/MakeAReservationPage";
+//components
+/* 
+============
+END IMPORTS
+===========
+*/
+
+export default function ReservationModal() {
+  return(
+    <>
+
+<div class="modal fade" id="reserveModal" tabindex="-1" role="dialog" aria-labelledby="reserveModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="reserveModalLabel">Select your dates</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p> hi, this is where the booking calendar will display.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </>
+  )
+};
--- src/pages/MakeAReservationPage.js
+++ src/pages/MakeAReservationPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 //pages
 //components
+import ReservationModal from "../components/ReservationModal";
 
 /* 
 ============
@@ -24,51 +26,53 @@ export default function MakeAReservationPage() {
           <div class="col-lg">
             <h5 class="card-title">The Cedar</h5>
               <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
-              <a href="#" class="card-link">view the Cedar</a>
-              <button type="button" class="btn btn-info " id="">Reserve now</button>
+              <a href="/thecedar" class="card-link">View the Cedar</a>
+              <button type="button" class="btn btn-info " id="" data-toggle="modal" data-target="#reserveModal">Reserve now</button>          
+          <ReservationModal />
           </div>
         </div>
+
         <div class="" id="">  
           <div class=" jumbotron container-fluid BookRmJumbo" id="">
             <div class="col-lg">
-              <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1603198132334-5478a1ec5bc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
             </div>
             <div class="col-lg">
-              <h5 class="card-title">The Cedar</h5>
+              <h5 class="card-title">The Willow</h5>
               <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
-              <a href="#" class="card-link">view the Cedar</a>
+              <a href="/thewillow" class="card-link">View the Willow</a>
               <button type="button" class="btn btn-info " id="">Reserve now</button>
             </div>
           </div>
         </div>
+
         <div class="" id="">  
           <div class=" jumbotron container-fluid BookRmJumbo" id="">
             <div class="col-lg">
-              <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1574197634772-f0de0d41a02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
             </div>
             <div class="col-lg">
-              <h5 class="card-title">The Cedar</h5>
+              <h5 class="card-title">The Sagebush</h5>
               <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
-              <a href="#" class="card-link">view the Cedar</a>
+              <a href="/thesagebush" class="card-link">View the Sagebush</a>
               <button type="button" class="btn btn-info " id="">Reserve now</button>
             </div>
           </div>
         </div>
+
         <div class="" id="">  
           <div class=" jumbotron container-fluid BookRmJumbo" id="">
             <div class="col-lg">
-              <img src="https://images.unsplash.com/photo-1471623600634-4d04cfc56a27?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1594900240017-d99eeb660287?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="img-fluid bookRmImg " alt="Responsive image" />
             </div>
             <div class="col-lg">
-              <h5 class="card-title">The Cedar</h5>
+              <h5 class="card-title">The Aloe</h5>
               <p class="card-text"> King bed. Full amenities access. Balcony view.</p>
-              <a href="#" class="card-link">view the Cedar</a>
+              <a href="/thealoe" class="card-link">view the Aloe</a>
               <button type="button" class="btn btn-info " id="">Reserve now</button>
             </div>
           </div>
-        </div>
-                                  
-      
+        </div>      
         
 
     </div>
