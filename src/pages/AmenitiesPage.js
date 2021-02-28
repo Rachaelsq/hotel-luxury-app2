ba444b811a870a8bdf4d2b1b270a4a4765721272
Rachaelsq <rachaelqsimone@gmail.com>
2021-02-28 14:17:27 -0600

    minor image swaps

--- src/pages/AmenitiesPage.js
+++ src/pages/AmenitiesPage.js
@@ -20,28 +20,35 @@ return (
     <h3 class="justify-content-center" id="amenitiesTitle">Amenities </h3>
       <div class="container-fluid" id="AmenitiesBigContainer">
 
-      <div class="col-xl container-fluid" id="firstColumn">
+      <div class="col-xl container-fluid " id="firstColumn">
         <div class="row">
 
-          <div class="col-xl container-fluid ">
-            <div class="row row-no-padding" id="topSideImgId">
+          <div class="col-xl container-fluid  ">
+            <div class="row row-no-padding topRow" id="topSideImgId">
               <h3 class="container-fluid justify-content-center d-flex">Relax...</h3>
               <img src="https://images.unsplash.com/photo-1610882290669-102aadebb413?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" topSideImg d-flex " alt="Responsive image" />
             </div>
-            <div class="container-fluid justify-content-center d-flex" id="bottomAmenitiesId">
-              <img src="https://images.unsplash.com/photo-1567667593973-e5227038363c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid " alt="Responsive image" />
-              <img src="https://images.unsplash.com/photo-1600599067176-1f47e3b6fe47?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=798&q=80" class=" bottomImg container-fluid" alt="Responsive image" />
+
+            <h4 class="container-fluid justify-content-center d-flex">Golfing & hiking</h4>
+            <div class="container-fluid justify-content-center d-flex " id="bottomAmenitiesId">
+              <img src="https://images.unsplash.com/photo-1610916445778-09401a50a2b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid " alt="golf" />
+              <img src="https://images.unsplash.com/photo-1600599067176-1f47e3b6fe47?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=798&q=80" class=" bottomImg container-fluid" alt="hiking" />
             </div>
+            
+            <h4 class="container-fluid justify-content-center d-flex">50 restaurants to choose from</h4>
             <div class="container-fluid justify-content-center d-flex" id="bottomAmenitiesId">
-              <img src="https://images.unsplash.com/photo-1567667593973-e5227038363c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid " alt="Responsive image" />
-              <img src="https://images.unsplash.com/photo-1600599067176-1f47e3b6fe47?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=798&q=80" class=" bottomImg container-fluid" alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1600905802115-e185ef10fedb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=802&q=80" class=" bottomImg container-fluid " alt="cafe" />
+              <img src="https://images.unsplash.com/photo-1563019880-9b2ea5d89a12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid" alt="fine dining" />
             </div>
-          </div>
+        </div>
 
           <div class="col-xl container-fluid" id="middleColumn">
-            <div class="row row-no-padding">
+            
+            <div class="row row-no-padding ">
               <h3 class="container-fluid justify-content-center d-flex">Refresh...</h3>
-              <img src="https://images.unsplash.com/photo-1588504273983-667893edbe3d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" " alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1588504273983-667893edbe3d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="topRow " alt="Responsive image" />
+              <h4 class="container-fluid justify-content-center d-flex">Marina access & boat rental</h4>
+
                 <div class="col-lg d-flex justify-content-center" id="">
                 <img src="https://images.unsplash.com/photo-1584772126711-017fae29eadd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" class=" middleBottom " alt="Responsive image" />
                 </div>
@@ -49,17 +56,21 @@ return (
           </div>
 
           <div class="col-xl container-fluid" id="lastColumn">
-            <div class="row row-no-padding">
+            <div class="row row-no-padding topRow">
               <h3 class="container-fluid justify-content-center d-flex">Rejuvenate...</h3>
               <img src="https://images.unsplash.com/photo-1567807052979-c991f0635b54?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" d-flex " alt="Responsive image" />
             </div>
+
+            <h4 class="container-fluid justify-content-center d-flex">Yoga & spa</h4>
             <div class="container-fluid justify-content-center d-flex" id="bottomAmenitiesId">
-              <img src="https://images.unsplash.com/photo-1528123887526-0775584e2093?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
-              <img src="https://images.unsplash.com/photo-1536936114683-1e27e62e3e9c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nzd8fGhvdGVsfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
-            </div>                  
+              <img src="https://images.unsplash.com/photo-1590079083219-5bc40d5a9d69?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=833&q=80" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1485727749690-d091e8284ef3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
+            </div>   
+
+            <h4 class="container-fluid justify-content-center d-flex">Games & entertainment</h4>               
             <div class="container-fluid justify-content-center d-flex" id="bottomAmenitiesId">
-              <img src="https://images.unsplash.com/photo-1528123887526-0775584e2093?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
-              <img src="https://images.unsplash.com/photo-1536936114683-1e27e62e3e9c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nzd8fGhvdGVsfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=798&q=800" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1514891163508-4d0d04535922?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class=" bottomImg container-fluid rightPadding" alt="Responsive image" />
             </div>                  
           </div>
 
--- src/pages/TheCedar.js
+++ src/pages/TheCedar.js
@@ -26,14 +26,14 @@ export default function TheCedar() {
           </div>
 
 
-                    <h3 class="viewMore align-middle">More photos of The Willow</h3>
+                    <h3 class="viewMore align-middle">More photos of The Cedar</h3>
 
           <div class="row">
             <div class="col-lg ">
               <img src="https://images.unsplash.com/photo-1588091945254-e2191fefe7b7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60" class="img-fluid RmImg" alt="Responsive image" />
             </div>
             <div class="col-lg ">
-              <img src="https://images.unsplash.com/photo-1567225557594-88d73e55f2cb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80" class="img-fluid RmImg " alt="Responsive image" />
+              <img src="https://images.unsplash.com/photo-1549416878-30862a3e49e7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80" class="img-fluid RmImg " alt="Responsive image" />
             </div>
             <div class="col-lg ">
               <img src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80" class="img-fluid RmImg " alt="Responsive image" />
